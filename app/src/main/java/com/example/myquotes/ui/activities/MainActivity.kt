9c2b3f8b4cb3eafd8c5e7a28304ea27705fbb368
Kagiri11 <charlesmaish423@gmail.com>
2021-06-18 21:42:29 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/myquotes/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myquotes.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myquotes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
